5554222f1939d1ff43072fe7c6af23bc35af29e8
N-akademy <n.wachani@outlook.fr>
2019-11-19 17:51:24 +0100

    partie1 Norhen

---
+++ templates/header.php
@@ -0,0 +1,23 @@
+ <?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test php</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <div>
+        <ul class="topnav">
+            <li><a class="active" href="index.php">Accueil</a></li>
+            <li><a href="decouverte.php">Présentation</a></li>
+            <li><a href="api.php">Questionnaire</a></li>
+            <li><a href="game.php">Jeu</a></li>
+            <li><a class="right" href="form.php">Formulaire</a></li>
+        </ul>
+    </div>    
+</header>
