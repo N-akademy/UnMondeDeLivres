039b1bed5f07a143727d6cacf2fce48b98e76f91
N-akademy <n.wachani@outlook.fr>
2019-11-21 16:34:25 +0100

    pouet pouet

--- templates/header.php
+++ templates/header.php
@@ -1,27 +1,41 @@
+<?php 
+ session_start();
+ ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/index.css">
+    <title>Un Monde de Livre</title>
+    <link rel="stylesheet" href="css/style.css">
+    
+    
+    
 </head>
 <body>
-    
-
     <header>
-
         <div>
-            <ul class="topnav">
+            <ul class="topnav" >
+                
+<?php
+            if(isset($_SESSION) && !empty($_SESSION)){
+?>
                 <li><a class="active" href="index.php">Accueil</a></li>
-                <li><a href="presentation.php">Suggestions</a></li>
+                <li><a href="decouverte.php">Présentation</a></li>
                 <li><a href="api.php">Questionnaire</a></li>
-                <li><a href="game.php">Jeu</a></li>
-                <li><a class="right" href="form.php">Formulaire</a></li>
+                <li><a href="form.php">Formulaire</a></li>
+                <li><a href="game.php">Jeu</a></li>   
+<?php 
+               }else{
+?>
+            <li><a class="active" href="index.php">Accueil</a></li>
+            <li><a href="decouverte.php">Présentation</a></li>
+            <li><a href="api.php">Questionnaire</a></li>
+            <li><a href="form.php">Formulaire</a></li>
+<?php
+              }
+?>
             </ul>
-        </div>
-
-    
-
+        </div>    
     </header>
